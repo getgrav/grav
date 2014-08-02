fab18e5468171c891fbde703b4117509812f5d1d
Andy Miller <rhuk@mac.com>
2014-08-02 15:09:18 -0600

    added auto-create for missing data/ folder

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ require_once(SYSTEM_DIR .'autoload.php');
 if (!file_exists(LOG_DIR)) mkdir(LOG_DIR);
 if (!file_exists(CACHE_DIR)) mkdir(CACHE_DIR);
 if (!file_exists(IMAGES_DIR)) mkdir(IMAGES_DIR);
+if (!file_exists(DATA_DIR)) mkdir(DATA_DIR);
 
 // Start the timer and enable debugger in production mode as we do not have system configuration yet.
 // Debugger catches all errors and logs them, for example if the script doesn't have write permissions.
