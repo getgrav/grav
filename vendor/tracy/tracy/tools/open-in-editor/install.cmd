57ab99fb138c4cd1804a28012df0a94a79c3b883
Djamil Legato <djamil@djamil.it>
2014-09-05 12:56:43 -0700

    Getting away from providing vendor with the repo

--- vendor/tracy/tracy/tools/open-in-editor/install.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-:: This Windows batch file sets open-editor.js as handler for editor:// protocol
-
-if defined PROCESSOR_ARCHITEW6432 (set reg="%systemroot%\sysnative\reg.exe") else (set reg=reg)
-
-%reg% ADD HKCR\editor /ve /d "URL:editor Protocol" /f
-%reg% ADD HKCR\editor /v "URL Protocol" /d "" /f
-%reg% ADD HKCR\editor\shell\open\command /ve /d "wscript \"%~dp0open-editor.js\" \"%%1\"" /f
