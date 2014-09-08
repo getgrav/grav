fa92ec0141961ccdc7841bba97c16b0f50174a40
Andy Miller <rhuk@mac.com>
2014-09-07 19:57:14 -0600

    Added method to get user IP address

--- system/src/Grav/Common/Uri.php
+++ system/src/Grav/Common/Uri.php
@@ -298,4 +298,30 @@ class Uri
         // Return relative path.
         return substr($referrer, strlen($root));
     }
+
+    /**
+     * Retrun the IP address of the current user
+     *
+     * @return string ip address
+     */
+    public function ip()
+    {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+           $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+
+    }
 }
