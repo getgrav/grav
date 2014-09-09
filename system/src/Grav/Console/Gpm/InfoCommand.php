a3b76252d13b3f7c69f124946f8c3f4ff154d5eb
Djamil Legato <djamil@djamil.it>
2014-09-08 18:04:36 -0700

    Updated InfoCommand to use the new GPM class

--- system/src/Grav/Console/Gpm/InfoCommand.php
+++ system/src/Grav/Console/Gpm/InfoCommand.php
@@ -1,83 +1,67 @@
 <?php
 namespace Grav\Console\Gpm;
 
+use Grav\Common\GPM\GPM;
 use Grav\Console\ConsoleTrait;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 class InfoCommand extends Command {
     use ConsoleTrait;
 
     protected function configure() {
         $this
-        ->setName("info")
-        ->addOption(
+            ->setName("info")
+            ->addOption(
             'force',
             'f',
             InputOption::VALUE_NONE,
             'Force fetching the new data remotely'
         )
-        ->addArgument(
+            ->addArgument(
             'package',
             InputArgument::REQUIRED,
             'The package of which more informations are desired. Use the "index" command for a list of packages'
 
         )
-        ->setDescription("Shows more informations about a package")
-        ->setHelp('The <info>info</info> shows more informations about a package');
+            ->setDescription("Shows more informations about a package")
+            ->setHelp('The <info>info</info> shows more informations about a package');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute(InputInterface $input, OutputInterface $output) {
         $this->setupConsole($input, $output);
-
-        $userPackage  = false;
-        $this->data   = $this->fetchData();
+        $this->gpm = new GPM($this->input->getOption('force'));
 
         $this->output->writeln('');
 
-        $findPackage = strtolower($input->getArgument('package'));
-        foreach ($this->data as $type => $result) {
-            $result = json_decode($result)->results;
-            $name = $result->name;
+        $foundPackage = $this->gpm->findPackage($input->getArgument('package'));
 
-            foreach ($result->data as $index => $package) {
-                if (strtolower($package->name) == $findPackage || strtolower($package->slug) == $findPackage){
-                    $userPackage = array();
-                    $userPackage[$name] = $package;
-                }
-            }
-        }
-
-        if (!$userPackage){
-            $this->output->writeln("The package <cyan>'".$input->getArgument('package')."'</cyan> was not found in the Grav repository.");
+        if (!$foundPackage) {
+            $this->output->writeln("The package <cyan>'" . $input->getArgument('package') . "'</cyan> was not found in the Grav repository.");
             $this->output->writeln('');
             $this->output->writeln("You can list all the available packages by typing:");
-            $this->output->writeln("    <green>".$this->argv." index</green>");
+            $this->output->writeln("    <green>" . $this->argv . " index</green>");
             $this->output->writeln('');
             exit;
         }
 
-        $key = key($userPackage);
-        $this->output->writeln("Found package <cyan>'".$input->getArgument('package')."'</cyan> under the '<green>".$key."</green>' section");
+        $this->output->writeln("Found package <cyan>'" . $input->getArgument('package') . "'</cyan> under the '<green>" . ucfirst($foundPackage->package_type) . "</green>' section");
         $this->output->writeln('');
-        $this->output->writeln("<cyan>".$userPackage[$key]->name."</cyan>");
+        $this->output->writeln("<cyan>" . $foundPackage->name . "</cyan>");
         $this->output->writeln('');
-        $this->output->writeln("<white>".strip_tags($userPackage[$key]->description)."</white>");
+        $this->output->writeln("<white>" . strip_tags($foundPackage->description) . "</white>");
         $this->output->writeln('');
-        $this->output->writeln("<magenta>Author:</magenta>     ".$userPackage[$key]->author);
-        $this->output->writeln("<magenta>Version:</magenta>    ".$userPackage[$key]->version);
-        $this->output->writeln("<magenta>Path:</magenta>       ".$userPackage[$key]->install_path);
-        $this->output->writeln("<magenta>URL:</magenta>        ".$userPackage[$key]->url);
-        $this->output->writeln("<magenta>Download:</magenta>   ".$userPackage[$key]->download);
+        $this->output->writeln("<magenta>Author:</magenta>     " . $foundPackage->author);
+        $this->output->writeln("<magenta>Version:</magenta>    " . $foundPackage->version);
+        $this->output->writeln("<magenta>Path:</magenta>       " . $foundPackage->install_path);
+        $this->output->writeln("<magenta>URL:</magenta>        " . $foundPackage->url);
+        $this->output->writeln("<magenta>Download:</magenta>   " . $foundPackage->download);
         $this->output->writeln('');
         $this->output->writeln("You can install this package by typing:");
-        $this->output->writeln("    <green>".$this->argv." install</green> <cyan>".$userPackage[$key]->slug."</cyan>");
+        $this->output->writeln("    <green>" . $this->argv . " install</green> <cyan>" . $foundPackage->slug . "</cyan>");
         $this->output->writeln('');
 
     }
