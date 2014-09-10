25c53fa3656f26b03999910846a9d30ab4f19cf1
Andy Miller <rhuk@mac.com>
2014-09-10 11:29:49 -0600

    Added checkbox support

--- admin.php
+++ admin.php
@@ -172,7 +172,7 @@ class AdminPlugin extends Plugin
         $twig->twig_vars['admin'] = $this->admin;
 
         // fake grav update
-        $twig->twig_vars['grav_update'] = array('current'=>'0.9.1', 'available'=>'0.9.2');
+        $twig->twig_vars['grav_update'] = array('current'=>'0.9.1', 'available'=>'0.9.1');
 
         switch ($this->template) {
             case 'dashboard':
