aa9af76f021b2df62e6c62e5648c6526c84d8eea
Djamil Legato <djamil@djamil.it>
2014-09-17 17:08:36 -0700

    Internal cache for the class to speed up retrieving plugins and themes

--- system/src/Grav/Common/GPM/Local/Packages.php
+++ system/src/Grav/Common/GPM/Local/Packages.php
@@ -7,14 +7,20 @@ class Packages extends Iterator
 {
     private $plugins;
     private $themes;
+    protected static $cache;
 
     public function __construct()
     {
-        $plugins = new Plugins();
-        $themes  = new Themes();
+        // local cache to speed things up
+        if (!isset(self::$cache[__METHOD__])) {
+            self::$cache[__METHOD__] = [
+                'plugins' => new Plugins(),
+                'themes'  => new Themes()
+            ];
+        }
 
-        $this->plugins = $plugins;
-        $this->themes  = $themes;
+        $this->plugins = self::$cache[__METHOD__]['plugins'];
+        $this->themes  = self::$cache[__METHOD__]['themes'];
 
         $this->append(['plugins' => $this->plugins]);
         $this->append(['themes' => $this->themes]);
