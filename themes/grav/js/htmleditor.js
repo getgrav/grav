fb34d15e3cc6ae4e91760bf7aa199ec1ada66db2
Djamil Legato <djamil@djamil.it>
2014-09-18 18:02:30 -0700

    Fixed regexp to match only front matter and not getting confused with hr

--- themes/grav/js/htmleditor.js
+++ themes/grav/js/htmleditor.js
@@ -267,7 +267,7 @@
 
         render: function() {
 
-            this.currentvalue = this.editor.getValue().replace(/^---\n([^---]*)---\n{1,}/, '');
+            this.currentvalue = this.editor.getValue().replace(/^---([\s\S]*?)---\n{1,}/gm, '');
 
             // empty code
             if (!this.currentvalue) {
