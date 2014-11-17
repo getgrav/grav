19fc443518005e2f1ce3828a5ab4ce0ec7fbb1de
Andy Miller <rhuk@mac.com>
2014-11-17 11:14:05 -0700

    Fix for relative URLs in markdown on installs with no base_url

--- system/src/Grav/Common/Markdown/MarkdownGravLinkTrait.php
+++ system/src/Grav/Common/Markdown/MarkdownGravLinkTrait.php
@@ -113,7 +113,7 @@ trait MarkdownGravLinkTrait
     protected function convertUrl($markdown_url)
     {
         // if absolue and starts with a base_url move on
-        if ($this->base_url == '' || strpos($markdown_url, $this->base_url) === 0) {
+        if ($this->base_url != '' && strpos($markdown_url, $this->base_url) === 0) {
             $new_url = $markdown_url;
         // if its absolute with /
         } elseif (strpos($markdown_url, '/') === 0) {
