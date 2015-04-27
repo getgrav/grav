e1d655a3ac3e36b7a0bc6ef68ec2d1261f69588b
Gert <gert@gert.gert>
2015-04-27 12:40:30 +0200

    move backup code into Grav\Common

---
+++ system/src/Grav/Common/Backup/ZipBackup.php
@@ -0,0 +1,103 @@
+<?php
+namespace Grav\Common\Backup;
+
+use Grav\Common\GravTrait;
+use Grav\Common\Filesystem\Folder;
+
+/**
+ * The ZipBackup class lets you create simple zip-backups of a grav site
+ *
+ * @author RocketTheme
+ * @license MIT
+ */
+class ZipBackup
+{
+    use GravTrait;
+
+    public static function backup($destination = null, callable $messager = null)
+    {
+        if (!$destination) {
+            $destination = self::getGrav()['locator']->findResource('cache://', true);
+            $destination = $destination . DS . 'tmp/Grav-' . uniqid();
+            Folder::mkdir($destination);
+        }
+
+        $name = basename(GRAV_ROOT);
+
+        if (is_dir($destination)) {
+            $date = date('YmdHis', time());
+            $filename = $name . '-' . $date . '.zip';
+            $destination = rtrim($destination, DS) . DS . $filename;
+        }
+
+        $messager && $messager([
+            'type' => 'message',
+            'level' => 'info',
+            'message' => 'Creating new Backup "' . $destination . '"'
+        ]);
+        $messager && $messager([
+            'type' => 'message',
+            'level' => 'info',
+            'message' => ''
+        ]);
+
+        $zip = new \ZipArchive();
+        $zip->open($destination, \ZipArchive::CREATE);
+        $zip->addEmptyDir($name);
+
+        static::folderToZip(GRAV_ROOT, $zip, strlen(rtrim(GRAV_ROOT, DS) . DS), $messager);
+
+        $messager && $messager([
+            'type' => 'progress',
+            'percentage' => false,
+            'complete' => true
+        ]);
+
+        $messager && $messager([
+            'type' => 'message',
+            'level' => 'info',
+            'message' => ''
+        ]);
+        $messager && $messager([
+            'type' => 'message',
+            'level' => 'info',
+            'message' => 'Saving and compressing archive...'
+        ]);
+
+        $zip->close();
+
+        return $destination;
+    }
+
+    /**
+     * @param $folder
+     * @param $zipFile
+     * @param $exclusiveLength
+     * @param $messager
+     */
+    private static function folderToZip($folder, \ZipArchive &$zipFile, $exclusiveLength, callable $messager = null)
+    {
+        $handle = opendir($folder);
+        while (false !== $f = readdir($handle)) {
+            if ($f != '.' && $f != '..') {
+                $filePath = "$folder/$f";
+                // Remove prefix from file path before add to zip.
+                $localPath = substr($filePath, $exclusiveLength);
+                if (is_file($filePath)) {
+                    $zipFile->addFile($filePath, $localPath);
+
+                    $messager && $messager([
+                        'type' => 'progress',
+                        'percentage' => false,
+                        'complete' => false
+                    ]);
+                } elseif (is_dir($filePath)) {
+                    // Add sub-directory.
+                    $zipFile->addEmptyDir($localPath);
+                    static::folderToZip($filePath, $zipFile, $exclusiveLength, $messager);
+                }
+            }
+        }
+        closedir($handle);
+    }
+}
