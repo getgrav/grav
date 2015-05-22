9d05f0dfb30e7d294a233706a47f58c7b813fe14
Gert <gert@Gerts-MacBook-Pro.local>
2015-05-22 17:12:18 +0200

    add support for datetime field

---
+++ themes/grav/js/datepicker/init.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $('input[type="date"]').each(function() {
+        var $input = $(this),
+            min = $input.attr('min'),
+            max = $input.attr('max'),
+            regex, match,
+            kendoOptions = { format: "dd-MM-yyyy HH:mm", timeFormat: "HH:mm" };
+
+        if (min || max) {
+            regex = /(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})/;
+        }
+
+        if (min && (match = regex.exec(min))) {
+            kendoOptions.min = new Date(
+                (+match[3]),
+                (+match[2])-1,
+                (+match[1]),
+                (+match[4]),
+                (+match[5])
+            );
+        }
+
+        if (max && (match = regex.exec(max))) {
+            kendoOptions.max = new Date(
+                (+match[3]),
+                (+match[2])-1,
+                (+match[1]),
+                (+match[4]),
+                (+match[5])
+            );
+        }
+
+        $input.kendoDateTimePicker(kendoOptions);
+
+        // Reset when user manually types in invalid date
+        $input.on('change', function () {
+            var kWidget = $input.data('kendoDateTimePicker');
+            if (kWidget && kWidget.value() === null && $input.val()) {
+                kWidget.value($input.data('kendo-previous') || "");
+            } else {
+                $input.data('kendo-previous', kWidget.value() || "");
+            }
+        });
+    });
+});
