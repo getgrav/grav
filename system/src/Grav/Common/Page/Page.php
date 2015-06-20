d309491f06c56399ef52bde1a6a214f1ce1e2270
Sommerregen <sommerregen@users.noreply.github.com>
2015-06-20 13:54:57 +0200

    Modified `$page->summary()` to allow custom summary assignments

--- system/src/Grav/Common/Page/Page.php
+++ system/src/Grav/Common/Page/Page.php
@@ -61,6 +61,7 @@ class Page
     protected $header;
     protected $frontmatter;
     protected $content;
+    protected $summary;
     protected $raw_content;
     protected $pagination;
     protected $media;
@@ -164,6 +165,7 @@ class Page
             $this->id($this->modified().md5($this->filePath()));
             $this->header = null;
             $this->content = null;
+            $this->summary = null;
         }
         return $file ? $file->raw() : '';
     }
@@ -305,15 +307,14 @@ class Page
     /**
      * Get the summary.
      *
-     * @param int $size  Max summary size.
+     * @param  int|string  $var  Summary or max summary size.
+     *
      * @return string
      */
-    public function summary($size = null)
+    public function summary($var = null)
     {
         /** @var Config $config */
         $config = self::getGrav()['config']->get('site.summary');
-        $content = $this->content();
-
         if (isset($this->header->summary)) {
             $config = array_merge($config, $this->header->summary);
         }
@@ -323,9 +324,19 @@ class Page
             return $content;
         }
 
-        // Get summary size from site config's file
-        if (is_null($size)) {
-            $size = $config['size'];
+        // Set summary
+        if (is_string($var)) {
+            $this->summary = $var;
+            $var = null;
+        }
+
+        // Set up variables to process summary from page or from custom summary
+        if ($this->summary === null) {
+            $content = $this->content();
+            $summary_size = $this->summary_size;
+        } else {
+            $content = $this->summary;
+            $summary_size = mb_strlen($this->summary);
         }
 
         // Return calculated summary based on summary divider's position
@@ -333,16 +344,21 @@ class Page
         // Return entire page content on wrong/ unknown format
         if (!in_array($format, array('short', 'long'))) {
             return $content;
-        } elseif (($format === 'short') && isset($this->summary_size)) {
-            return substr($content, 0, $this->summary_size);
+        } elseif (($format === 'short') && isset($summary_size)) {
+            return mb_substr($content, 0, $summary_size);
+        }
+
+        // Get summary size from site config's file
+        if (is_null($var)) {
+            $var = $config['size'];
         }
 
         // If the size is zero, return the entire page content
-        if ($size === 0) {
+        if ($var === 0) {
             return $content;
         // Return calculated summary based on defaults
-        } elseif (!is_numeric($size) || ($size < 0)) {
-            $size = 300;
+        } elseif (!is_numeric($var) || ($var < 0)) {
+            $var = 300;
         }
 
         return Utils::truncateHTML($content, $size);
