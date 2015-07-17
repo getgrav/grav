5f32ea64ebe9f84a383647922a0b88aa65851fc7
Andy Miller <rhuk@mac.com>
2015-07-17 16:06:23 -0600

    Extended `ImageFile` to provide event on creation function

--- system/src/Grav/Common/Page/Medium/ImageMedium.php
+++ system/src/Grav/Common/Page/Medium/ImageMedium.php
@@ -1,9 +1,7 @@
 <?php
 namespace Grav\Common\Page\Medium;
 
-use RocketTheme\Toolbox\Event\Event;
 use Grav\Common\Data\Blueprint;
-use Gregwar\Image\Image as ImageFile;
 
 class ImageMedium extends Medium
 {
@@ -36,11 +34,6 @@ class ImageMedium extends Medium
      * @var boolean
      */
     protected $debug_watermarked = false;
-
-    /**
-     * @var string
-     */
-    public $result;
 
     /**
      * @var array
@@ -370,6 +363,10 @@ class ImageMedium extends Medium
             return parent::path(false);
         }
 
+        if (isset($this->result)) {
+            return $this->result;
+        }
+
         if ($this->get('debug') && !$this->debug_watermarked) {
             $ratio = $this->get('ratio');
             if (!$ratio) {
@@ -381,11 +378,7 @@ class ImageMedium extends Medium
             $this->image->merge(ImageFile::open($overlay));
         }
 
-        $this->result = $this->image->cacheFile($this->format, $this->quality);
-
-        self::getGrav()->fireEvent('onImageMediumSaved', new Event(['image' => $this]));
-
-        return $this->result;
+        return $this->image->cacheFile($this->format, $this->quality);
     }
 
     /**
