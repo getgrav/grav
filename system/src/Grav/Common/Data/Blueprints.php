getgrav/grav

commit 4a59b8ae4359040434f135fafdee4f839853bb67

Fix logic to list and load blueprints by using streams (#38)

system/src/Grav/Common/Data/Blueprints.php
@@ -3,6 +3,7 @@ namespace Grav\Common\Data;
 
 use Grav\Common\File\CompiledYamlFile;
 use Grav\Common\GravTrait;
+use RocketTheme\Toolbox\ResourceLocator\UniformResourceLocator;
 
 /**
  * Blueprints class keeps track on blueprint instances.
@@ -23,11 +24,7 @@ class Blueprints
      */
     public function __construct($search)
     {
-        if (!is_string($search)) {
-            $this->search = $search;
-        } else {
-            $this->search = rtrim($search, '\\/') . '/';
-        }
+        $this->search = $search;
     }
 
     /**
@@ -42,6 +39,14 @@ class Blueprints
         if (!isset($this->instances[$type])) {
             if (is_string($this->search)) {
                 $filename = $this->search . $type . YAML_EXT;
+
+                // Check if search is a stream and resolve the path.
+                if (strpos($filename, '://')) {
+                    $grav = static::getGrav();
+                    /** @var UniformResourceLocator $locator */
+                    $locator = $grav['locator'];
+                    $filename = $locator($filename);
+                }
             } else {
                 $filename = isset($this->search[$type]) ? $this->search[$type] : '';
             }
@@ -91,7 +96,18 @@ class Blueprints
         if ($this->types === null) {
             $this->types = array();
 
-            $iterator   = new \DirectoryIterator($this->search);
+            // Check if search is a stream.
+            if (strpos($this->search, '://')) {
+                // Stream: use UniformResourceIterator.
+                $grav = static::getGrav();
+                /** @var UniformResourceLocator $locator */
+                $locator = $grav['locator'];
+                $iterator = $locator->getIterator($this->search, null);
+            } else {
+                // Not a stream: use DirectoryIterator.
+                $iterator = new \DirectoryIterator($this->search);
+            }
+
             /** @var \DirectoryIterator $file */
             foreach ($iterator as $file) {
                 if (!$file->isFile() || '.' . $file->getExtension() != YAML_EXT) {

commit 26d19731101ff383bc770e008a0c07875a3a8774

Fix blueprints loading (regression from #38)

system/src/Grav/Common/Data/Blueprints.php
@@ -37,6 +37,7 @@ class Blueprints
     public function get($type)
     {
         if (!isset($this->instances[$type])) {
+            $parents = [];
             if (is_string($this->search)) {
                 $filename = $this->search . $type . YAML_EXT;
 

system/src/Grav/Common/Plugins.php
@@ -113,8 +113,8 @@ class Plugins extends Iterator
 
     public static function get($name)
     {
-        $blueprints = new Blueprints("plugins://{$name}");
-        $blueprint = $blueprints->get('blueprints');
+        $blueprints = new Blueprints('plugins://');
+        $blueprint = $blueprints->get("{$name}/blueprints");
         $blueprint->name = $name;
 
         // Load default configuration.

system/src/Grav/Common/Themes.php
@@ -90,8 +90,8 @@ class Themes extends Iterator
             throw new \RuntimeException('Theme name not provided.');
         }
 
-        $blueprints = new Blueprints("themes://{$name}");
-        $blueprint = $blueprints->get('blueprints');
+        $blueprints = new Blueprints('themes://');
+        $blueprint = $blueprints->get("{$name}/blueprints");
         $blueprint->name = $name;
 
         // Find thumbnail.

system/src/Grav/Common/User/User.php
@@ -32,8 +32,8 @@ class User extends Data
         $locator = self::getGrav()['locator'];
 
         // TODO: validate directory name
-        $blueprints = new Blueprints('blueprints://user');
-        $blueprint = $blueprints->get('account');
+        $blueprints = new Blueprints('blueprints://');
+        $blueprint = $blueprints->get('user/account');
         $file_path = $locator->findResource('account://' . $username . YAML_EXT);
         $file = CompiledYamlFile::instance($file_path);
         $content = $file->content();