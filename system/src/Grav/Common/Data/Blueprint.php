getgrav/grav

commit 4a59b8ae4359040434f135fafdee4f839853bb67

Fix logic to list and load blueprints by using streams (#38)

system/src/Grav/Common/Data/Blueprints.php
@@ -3,6 +3,7 @@ namespace Grav\Common\Data;
 
 use Grav\Common\File\CompiledYamlFile;
 use Grav\Common\GravTrait;
+use RocketTheme\Toolbox\ResourceLocator\UniformResourceLocator;
 
 /**
  * Blueprints class keeps track on blueprint instances.
@@ -23,11 +24,7 @@ class Blueprints
      */
     public function __construct($search)
     {
-        if (!is_string($search)) {
-            $this->search = $search;
-        } else {
-            $this->search = rtrim($search, '\\/') . '/';
-        }
+        $this->search = $search;
     }
 
     /**
@@ -42,6 +39,14 @@ class Blueprints
         if (!isset($this->instances[$type])) {
             if (is_string($this->search)) {
                 $filename = $this->search . $type . YAML_EXT;
+
+                // Check if search is a stream and resolve the path.
+                if (strpos($filename, '://')) {
+                    $grav = static::getGrav();
+                    /** @var UniformResourceLocator $locator */
+                    $locator = $grav['locator'];
+                    $filename = $locator($filename);
+                }
             } else {
                 $filename = isset($this->search[$type]) ? $this->search[$type] : '';
             }
@@ -91,7 +96,18 @@ class Blueprints
         if ($this->types === null) {
             $this->types = array();
 
-            $iterator   = new \DirectoryIterator($this->search);
+            // Check if search is a stream.
+            if (strpos($this->search, '://')) {
+                // Stream: use UniformResourceIterator.
+                $grav = static::getGrav();
+                /** @var UniformResourceLocator $locator */
+                $locator = $grav['locator'];
+                $iterator = $locator->getIterator($this->search, null);
+            } else {
+                // Not a stream: use DirectoryIterator.
+                $iterator = new \DirectoryIterator($this->search);
+            }
+
             /** @var \DirectoryIterator $file */
             foreach ($iterator as $file) {
                 if (!$file->isFile() || '.' . $file->getExtension() != YAML_EXT) {

commit 056ad33f5038c2eb097b1e6091b278614a1e6f50

Fix blueprint @extends with external context (regression from #38)

system/src/Grav/Common/Data/Blueprint.php
@@ -324,7 +324,7 @@ class Blueprint
             $field['name'] = $prefix . $key;
             $field += $params;
 
-            if (isset($field['fields']) && $field['type'] !== 'list') {
+            if (isset($field['fields']) && (!isset($field['type']) || $field['type'] !== 'list')) {
                 // Recursively get all the nested fields.
                 $newParams = array_intersect_key($this->filter, $field);
                 $this->parseFormFields($field['fields'], $newParams, $prefix, $current[$key]['fields']);

system/src/Grav/Common/Data/Blueprints.php
@@ -86,7 +86,19 @@ class Blueprints
                         continue;
                     }
 
-                    $context = is_string($extendConfig) || empty($extendConfig['context']) ? $this : new self(self::getGrav()['locator']->findResource($extendConfig['context']));
+                    if (is_string($extendConfig) || empty($extendConfig['context'])) {
+                        $context = $this;
+                    } else {
+                        // Load blueprints from external context.
+                        $array = explode('://', $extendConfig['context'], 2);
+                        $scheme = array_shift($array);
+                        $path = array_shift($array);
+                        if ($path) {
+                            $scheme .= '://';
+                            $extendType = $path ? "{$path}/{$extendType}" : $extendType;
+                        }
+                        $context = new self($scheme);
+                    }
                     $blueprint->extend($context->get($extendType));
                 }
             }