e0d51beb8406a20e177e539f4f9094cdb59a8b33
Andy Miller <rhuk@mac.com>
2015-10-02 16:58:34 -0600

    PHP 5.4 fix

--- system/src/Grav/Common/Browser.php
+++ system/src/Grav/Common/Browser.php
@@ -44,11 +44,12 @@ class Browser
      */
     public function isHuman()
     {
-        if (empty($this->getBrowser())) {
+        $browser = $this->getBrowser();
+        if (empty($browser)) {
             return false;
         }
 
-        if (preg_match('~(bot|crawl)~i', $this->getBrowser())) {
+        if (preg_match('~(bot|crawl)~i', $browser)) {
             return false;
         }
 
