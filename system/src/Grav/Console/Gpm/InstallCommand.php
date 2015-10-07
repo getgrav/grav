ce8e985e56f97be8014f24eee5b7451e0491f178
Andy Miller <rhuk@mac.com>
2015-10-07 16:53:00 -0600

    Added a check in `bin/gpm install` command that could cause open_basedir errors.

--- system/src/Grav/Console/Gpm/InstallCommand.php
+++ system/src/Grav/Console/Gpm/InstallCommand.php
@@ -102,7 +102,7 @@ class InstallCommand extends Command
         $packages = array_map('strtolower', $this->input->getArgument('package'));
         $this->data = $this->gpm->findPackages($packages);
 
-        if (false === $this->isWindows()) {
+        if (false === $this->isWindows() && @is_file(getenv("HOME").'/.grav/config')) {
             $local_config_file = exec('eval echo ~/.grav/config');
             if (file_exists($local_config_file)) {
                 $this->local_config = Yaml::parse($local_config_file);
