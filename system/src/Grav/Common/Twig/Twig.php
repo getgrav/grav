77db54c50dbf379eaae5d41eff98706e127cbeba
Flavio Copes <copesc@gmail.com>
2015-11-25 22:50:29 +0100

    Revert "If the page does not exist trigger a 404"
    This reverts commit 8d8420c0d66395b47337e6ee54bb46b498660969.

--- system/src/Grav/Common/Twig/Twig.php
+++ system/src/Grav/Common/Twig/Twig.php
@@ -297,11 +297,6 @@ class Twig
         $this->grav->fireEvent('onTwigSiteVariables');
         $pages = $this->grav['pages'];
         $page = $this->grav['page'];
-
-        if (!$page) {
-            throw new \RuntimeException('Page Not Found', 404);
-        }
-
         $content = $page->content();
         $config = $this->grav['config'];
 
