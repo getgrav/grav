7b116b41ae10df1188766d3cc1ddcf49fc662f5d
Djamil Legato <djamil@djamil.it>
2015-12-11 20:42:30 -0800

    Not meant to be checked in

--- system/tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-error_reporting(E_ALL);
-date_default_timezone_set(@date_default_timezone_get());
-require_once __DIR__.'/../../vendor/autoload.php';
-require_once __DIR__.'/Grav/TestCase.php';
