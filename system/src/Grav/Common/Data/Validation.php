451baff26ee28612f49fc9d46a349f8205bc2cfa
Flavio Copes <copesc@gmail.com>
2015-12-16 15:16:14 +0100

    filterFile if the file field allows multiple items returns an array, otherwise a string

--- system/src/Grav/Common/Data/Validation.php
+++ system/src/Grav/Common/Data/Validation.php
@@ -283,7 +283,11 @@ class Validation
 
     protected static function filterFile($value, array $params, array $field)
     {
-        return (array) $value;
+        if ($field['multiple'] == true) {
+            return (array) $value;
+        }
+
+        return reset($value);
     }
 
     /**
