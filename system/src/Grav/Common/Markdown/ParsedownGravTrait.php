getgrav/grav

commit 23a9a736002cdcac08f22810f2e74ce2a9772a4d

Added logic to support link attributes via query string

system/src/Grav/Common/Markdown/ParsedownGravTrait.php
@@ -204,6 +204,48 @@ trait ParsedownGravTrait
         if (isset($excerpt['element']['attributes']['href'])) {
             $url = parse_url(htmlspecialchars_decode($excerpt['element']['attributes']['href']));
 
+            // if there is a query, then parse it and build action calls
+            if (isset($url['query'])) {
+                $actions = array_reduce(explode('&', $url['query']), function ($carry, $item) {
+                    $parts = explode('=', $item, 2);
+                    $value = isset($parts[1]) ? $parts[1] : null;
+                    $carry[$parts[0]] = $value;
+
+                    return $carry;
+                }, []);
+
+                // valid attributes supported
+                $valid_attributes = ['rel', 'target', 'id', 'class', 'classes'];
+
+                // Unless told to not process, go through actions
+                if (array_key_exists('noprocess', $actions)) {
+                    unset($actions['noprocess']);
+                } else {
+                    // loop through actions for the image and call them
+                    foreach ($actions as $attrib => $value) {
+                        $key = $attrib;
+
+                        if (in_array($attrib, $valid_attributes)) {
+                            // support both class and classes
+                            if ($attrib == 'classes') {
+                                $attrib = 'class';
+                            }
+                            $excerpt['element']['attributes'][$attrib] = $value;
+                            unset($actions[$key]);
+                        }
+                    }
+                }
+
+
+                $url['query']= http_build_query($actions, null, '&', PHP_QUERY_RFC3986);
+            }
+
+            // if no query elements left, unset query
+            if (empty($url['query'])) {
+                unset ($url['query']);
+            }
+
+
             // if there is no scheme, the file is local
             if (!isset($url['scheme']) && (count($url) > 0)) {
                 // convert the URl is required

commit 729f83b3d409c703b1c28efabfc3927be3878de8

Fix regression in image parameters handling introduced in https://github.com/getgrav/grav/commit/23a9a736002cdcac08f22810f2e74ce2a9772a4d
Parameters without a value (e.g. `sepia`, `grayscale`, `emboss` and
others) were not applied any more)

system/src/Grav/Common/Markdown/ParsedownGravTrait.php
@@ -272,7 +272,7 @@ trait ParsedownGravTrait
             if (isset($url['query'])) {
                 $actions = array_reduce(explode('&', $url['query']), function ($carry, $item) {
                     $parts = explode('=', $item, 2);
-                    $value = isset($parts[1]) ? $parts[1] : null;
+                    $value = isset($parts[1]) ? $parts[1] : true;
                     $carry[$parts[0]] = $value;
 
                     return $carry;