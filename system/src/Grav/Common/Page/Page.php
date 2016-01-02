4d396fb9f2e31907c0ac8b9754ef0e0cd76c94d7
franchan <franchan@loir>
2016-01-02 11:31:04 +0100

    Add onPageContentFromCache event
    This event notifies plugins that content came from the cache.
    It allows plugins to inspect this cached content and
    decide if specific plugin related assets need to be added.

--- system/src/Grav/Common/Page/Page.php
+++ system/src/Grav/Common/Page/Page.php
@@ -520,6 +520,9 @@ class Page
                 if ($cache_enable) {
                     $this->cachePageContent();
                 }
+            } else {
+                // Inform plugins that content was cached
+                self::getGrav()->fireEvent('onPageContentFromCache', new Event(['page' => $this]));                
             }
 
             // Handle summary divider
