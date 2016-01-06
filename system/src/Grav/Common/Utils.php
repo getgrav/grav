95c09f2ce561c5f6789f9561f528ce5f9e3c8805
Flavio Copes <copesc@gmail.com>
2016-01-06 23:34:28 +0100

    Add HTTP_CLIENT_IP too

--- system/src/Grav/Common/Utils.php
+++ system/src/Grav/Common/Utils.php
@@ -454,7 +454,9 @@ abstract class Utils
      */
     private static function generateNonceString($action, $plusOneTick = false)
     {
-        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
             $ip = $_SERVER['REMOTE_ADDR'];
