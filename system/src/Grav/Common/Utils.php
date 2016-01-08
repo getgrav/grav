8cb098c6e0102073713bea8017a26f7321b9bc44
Andy Miller <rhuk@mac.com>
2016-01-08 15:28:03 -0700

    Removed IP stuff from nonce.. just too many scenarios where it causes problems.  going to rely on session_id() for the uniqueness.

--- system/src/Grav/Common/Utils.php
+++ system/src/Grav/Common/Utils.php
@@ -446,6 +446,8 @@ abstract class Utils
 
     /**
      * Generates a nonce string to be hashed. Called by self::getNonce()
+     * We removed the IP portion in this version because it causes too many inconsistencies
+     * with reverse proxy setups.
      *
      * @param string $action
      * @param bool $plusOneTick if true, generates the token for the next tick (the next 12 hours)
@@ -454,21 +456,11 @@ abstract class Utils
      */
     private static function generateNonceString($action, $plusOneTick = false)
     {
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-
         $username = '';
         if (isset(self::getGrav()['user'])) {
             $user = self::getGrav()['user'];
             $username = $user->username;
         }
-
-        $username .= $ip;
 
         $token = session_id();
         $i = self::nonceTick();
