e1f95b54d5f906140d3741fc7083118c86042e53
Flavio Copes <copesc@gmail.com>
2016-01-15 21:47:05 +0100

    Initial testing on the Uri class. Test the new validateHostname() method and tests initializeWithUrl() with some url examples

---
+++ tests/unit/Grav/Common/UriTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use Codeception\Util\Fixtures;
+use Grav\Common\Uri;
+
+class UriTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+        $uri = $this->getURI();
+    }
+
+    protected function _after()
+    {
+    }
+
+    public function grav()
+    {
+        $grav = Fixtures::get('grav');
+        return $grav;
+    }
+
+    public function getURI()
+    {
+        return $this->grav()['uri'];
+    }
+
+    public function testValidatingHostname()
+    {
+        $uri = $this->getURI();
+
+        $this->assertTrue($uri->validateHostname('localhost') == 1);
+        $this->assertTrue($uri->validateHostname('google.com') == 1);
+        $this->assertTrue($uri->validateHostname('google.it') == 1);
+        $this->assertTrue($uri->validateHostname('goog.le') == 1);
+        $this->assertTrue($uri->validateHostname('goog.wine') == 1);
+        $this->assertTrue($uri->validateHostname('goog.localhost') == 1);
+
+        $this->assertFalse($uri->validateHostname('localhost:80') == 1);
+        $this->assertFalse($uri->validateHostname('http://localhost') == 1);
+        $this->assertFalse($uri->validateHostname('localhost!') == 1);
+    }
+
+    public function testInitializingUris()
+    {
+        $uri = $this->getURI();
+
+        $uri->initializeWithURL('http://localhost:8080/grav/it/ueper');
+        $uri->init();
+
+        $this->assertTrue($uri->params() == null);
+        $this->assertTrue($uri->query() == '');
+
+        $uri->initializeWithURL('http://localhost:8080/grav/it/ueper:xxx');
+        $uri->init();
+
+        $this->assertFalse($uri->params() == null);
+        $this->assertTrue($uri->query() == '');
+
+        $uri->initializeWithURL('http://localhost:8080/grav/it/ueper?test=x');
+        $uri->init();
+
+        $this->assertTrue($uri->params() == null);
+        $this->assertTrue($uri->query() != '');
+        $this->assertTrue($uri->query() == 'test=x');
+    }
+
+}
+
