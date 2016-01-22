27af9432e0fa9a7abb2b6a2e3f3a6f6d155a37e9
Flavio Copes <copesc@gmail.com>
2016-01-22 10:38:56 +0100

    :rotating_light: Added a bunch of tests to make sure Assets addJs and addCSS works as expected

--- tests/unit/Grav/Common/AssetsTest.php
+++ tests/unit/Grav/Common/AssetsTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Codeception\Util\Fixtures;
-use Grav\Common\Utils;
 
 class AssetsTest extends \Codeception\TestCase\Test
 {
@@ -37,23 +36,23 @@ class AssetsTest extends \Codeception\TestCase\Test
         $assets->add('test.css');
 
         $css = $assets->css();
-        $this->assertSame($css, '<link href="test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+        $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
 
         $assets->add('test.js');
         $js = $assets->js();
-        $this->assertSame($js, '<script src="test.js" type="text/javascript" ></script>' . PHP_EOL);
+        $this->assertSame($js, '<script src="/test.js" type="text/javascript" ></script>' . PHP_EOL);
 
         //test addCss(). Test adding asset to a separate group
         $assets->reset();
         $assets->addCSS('test.css');
         $css = $assets->css();
-        $this->assertSame($css, '<link href="test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+        $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
 
         //test addJs()
         $assets->reset();
         $assets->addJs('test.js');
         $js = $assets->js();
-        $this->assertSame($js, '<script src="test.js" type="text/javascript" ></script>' . PHP_EOL);
+        $this->assertSame($js, '<script src="/test.js" type="text/javascript" ></script>' . PHP_EOL);
 
         //Test CSS Groups
         $assets->reset();
@@ -61,7 +60,7 @@ class AssetsTest extends \Codeception\TestCase\Test
         $css = $assets->css();
         $this->assertEmpty($css);
         $css = $assets->css('footer');
-        $this->assertSame($css, '<link href="test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+        $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
 
         //Test JS Groups
         $assets->reset();
@@ -69,19 +68,101 @@ class AssetsTest extends \Codeception\TestCase\Test
         $js = $assets->js();
         $this->assertEmpty($js);
         $js = $assets->js('footer');
-        $this->assertSame($js, '<script src="test.js" type="text/javascript" ></script>' . PHP_EOL);
+        $this->assertSame($js, '<script src="/test.js" type="text/javascript" ></script>' . PHP_EOL);
 
         //Test async / defer
         $assets->reset();
         $assets->addJs('test.js', null, true, 'async', null);
         $js = $assets->js();
-        $this->assertSame($js, '<script src="test.js" type="text/javascript" async></script>' . PHP_EOL);
+        $this->assertSame($js, '<script src="/test.js" type="text/javascript" async></script>' . PHP_EOL);
         $assets->reset();
         $assets->addJs('test.js', null, true, 'defer', null);
         $js = $assets->js();
-        $this->assertSame($js, '<script src="test.js" type="text/javascript" defer></script>' . PHP_EOL);
+        $this->assertSame($js, '<script src="/test.js" type="text/javascript" defer></script>' . PHP_EOL);
 
 
+    }
+
+    public function testAddingAssetPropertiesWithArray()
+    {
+        $assets = $this->assets();
+
+        //Test adding assets with object to define properties
+        $assets->reset();
+        $assets->addJs('test.js', ['loading' => 'async']);
+        $js = $assets->js();
+        $this->assertSame($js, '<script src="/test.js" type="text/javascript" async></script>' . PHP_EOL);
+        $assets->reset();
+
+    }
+
+    public function testAddingJSAssetPropertiesWithArrayFromCollection()
+    {
+        $assets = $this->assets();
+
+        //Test adding properties with array
+        $assets->reset();
+        $assets->addJs('jquery', ['loading' => 'async']);
+        $js = $assets->js();
+        $this->assertSame($js, '<script src="/system/assets/jquery/jquery-2.1.4.min.js" type="text/javascript" async></script>' . PHP_EOL);
+
+        //Test priority too
+        $assets->reset();
+        $assets->addJs('jquery', ['loading' => 'async', 'priority' => 1]);
+        $assets->addJs('test.js', ['loading' => 'async', 'priority' => 2]);
+        $js = $assets->js();
+        $this->assertSame($js, '<script src="/test.js" type="text/javascript" async></script>' . PHP_EOL .
+            '<script src="/system/assets/jquery/jquery-2.1.4.min.js" type="text/javascript" async></script>' . PHP_EOL);
+
+        //Test multiple groups
+        $assets->reset();
+        $assets->addJs('jquery', ['loading' => 'async', 'priority' => 1, 'group' => 'footer']);
+        $assets->addJs('test.js', ['loading' => 'async', 'priority' => 2]);
+        $js = $assets->js();
+        $this->assertSame($js, '<script src="/test.js" type="text/javascript" async></script>' . PHP_EOL);
+        $js = $assets->js('footer');
+        $this->assertSame($js, '<script src="/system/assets/jquery/jquery-2.1.4.min.js" type="text/javascript" async></script>' . PHP_EOL);
+
+        //Test adding array of assets
+        //Test priority too
+        $assets->reset();
+        $assets->addJs(['jquery', 'test.js'], ['loading' => 'async']);
+        $js = $assets->js();
+
+        $this->assertSame($js, '<script src="/system/assets/jquery/jquery-2.1.4.min.js" type="text/javascript" async></script>' . PHP_EOL .
+            '<script src="/test.js" type="text/javascript" async></script>' . PHP_EOL);
+    }
+
+    public function testAddingCSSAssetPropertiesWithArrayFromCollection()
+    {
+        $assets = $this->assets();
+
+        $assets->registerCollection('test', ['/system/assets/whoops.css']);
+
+        //Test priority too
+        $assets->reset();
+        $assets->addCss('test', ['priority' => 1]);
+        $assets->addCss('test.css', ['priority' => 2]);
+        $css = $assets->css();
+        $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
+            '<link href="/system/assets/whoops.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+
+        //Test multiple groups
+        $assets->reset();
+        $assets->addCss('test', ['priority' => 1, 'group' => 'footer']);
+        $assets->addCss('test.css', ['priority' => 2]);
+        $css = $assets->css();
+        $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+        $css = $assets->css('footer');
+        $this->assertSame($css, '<link href="/system/assets/whoops.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+
+        //Test adding array of assets
+        //Test priority too
+        $assets->reset();
+        $assets->addCss(['test', 'test.css'], ['loading' => 'async']);
+        $css = $assets->css();
+        $this->assertSame($css, '<link href="/system/assets/whoops.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
+            '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
     }
 
     public function testPriorityOfAssets()
@@ -93,8 +174,8 @@ class AssetsTest extends \Codeception\TestCase\Test
         $assets->add('test-after.css');
 
         $css = $assets->css();
-        $this->assertSame($css, '<link href="test.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
-            '<link href="test-after.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+        $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
+            '<link href="/test-after.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
 
         //----------------
         $assets->reset();
@@ -102,8 +183,8 @@ class AssetsTest extends \Codeception\TestCase\Test
         $assets->add('test.css', 2);
 
         $css = $assets->css();
-        $this->assertSame($css, '<link href="test.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
-            '<link href="test-after.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+        $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
+            '<link href="/test-after.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
 
         //----------------
         $assets->reset();
@@ -112,9 +193,9 @@ class AssetsTest extends \Codeception\TestCase\Test
         $assets->add('test-before.css', 3);
 
         $css = $assets->css();
-        $this->assertSame($css, '<link href="test-before.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
-            '<link href="test.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
-            '<link href="test-after.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
+        $this->assertSame($css, '<link href="/test-before.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
+            '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL .
+            '<link href="/test-after.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
     }
 
     public function testPipeline()
