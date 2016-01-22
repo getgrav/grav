dd43b3b7da805a0b749a3aa3d071df3a6d7f8bf4
Flavio Copes <copesc@gmail.com>
2016-01-22 10:39:15 +0100

    :rotating_light: Remove acceptance test example

--- tests/acceptance/GravCept.php
+++
@@ -1,7 +0,0 @@
-<?php
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
-$I->amOnPage('/');
-$I->seeInTitle("Home | Grav");
-$I->click('ul.navigation li:nth-child(6) a');
-$I->seeCurrentUrlEquals('/grav/typography');
