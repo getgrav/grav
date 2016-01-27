c3c7c78e465a23dbb3776a22ff6be9adc46eb152
Flavio Copes <copesc@gmail.com>
2016-01-27 09:25:07 +0100

    :white_check_mark: Test Assets::getCss() and Assets::getJs()

--- tests/unit/Grav/Common/AssetsTest.php
+++ tests/unit/Grav/Common/AssetsTest.php
@@ -38,9 +38,27 @@ class AssetsTest extends \Codeception\TestCase\Test
         $css = $assets->css();
         $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
 
+        $array = $assets->getCss();
+        $this->assertSame(reset($array), [
+            'asset' => '/test.css',
+            'priority' => 10,
+            'order' => 0,
+            'pipeline' => true,
+            'group' => 'head'
+        ]);
+
         $assets->add('test.js');
         $js = $assets->js();
         $this->assertSame($js, '<script src="/test.js" type="text/javascript" ></script>' . PHP_EOL);
+
+        $array = $assets->getCss();
+        $this->assertSame(reset($array), [
+            'asset' => '/test.css',
+            'priority' => 10,
+            'order' => 0,
+            'pipeline' => true,
+            'group' => 'head'
+        ]);
 
         //test addCss(). Test adding asset to a separate group
         $assets->reset();
@@ -48,11 +66,30 @@ class AssetsTest extends \Codeception\TestCase\Test
         $css = $assets->css();
         $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
 
+        $array = $assets->getCss();
+        $this->assertSame(reset($array), [
+            'asset' => '/test.css',
+            'priority' => 10,
+            'order' => 0,
+            'pipeline' => true,
+            'group' => 'head'
+        ]);
+
         //test addJs()
         $assets->reset();
         $assets->addJs('test.js');
         $js = $assets->js();
         $this->assertSame($js, '<script src="/test.js" type="text/javascript" ></script>' . PHP_EOL);
+
+        $array = $assets->getJs();
+        $this->assertSame(reset($array), [
+            'asset' => '/test.js',
+            'priority' => 10,
+            'order' => 0,
+            'pipeline' => true,
+            'loading' => '',
+            'group' => 'head'
+        ]);
 
         //Test CSS Groups
         $assets->reset();
@@ -62,6 +99,15 @@ class AssetsTest extends \Codeception\TestCase\Test
         $css = $assets->css('footer');
         $this->assertSame($css, '<link href="/test.css" type="text/css" rel="stylesheet" />' . PHP_EOL);
 
+        $array = $assets->getCss();
+        $this->assertSame(reset($array), [
+            'asset' => '/test.css',
+            'priority' => 10,
+            'order' => 0,
+            'pipeline' => true,
+            'group' => 'footer'
+        ]);
+
         //Test JS Groups
         $assets->reset();
         $assets->addJs('test.js', null, true, null, 'footer');
@@ -70,17 +116,46 @@ class AssetsTest extends \Codeception\TestCase\Test
         $js = $assets->js('footer');
         $this->assertSame($js, '<script src="/test.js" type="text/javascript" ></script>' . PHP_EOL);
 
+        $array = $assets->getJs();
+        $this->assertSame(reset($array), [
+            'asset' => '/test.js',
+            'priority' => 10,
+            'order' => 0,
+            'pipeline' => true,
+            'loading' => '',
+            'group' => 'footer'
+        ]);
+
         //Test async / defer
         $assets->reset();
         $assets->addJs('test.js', null, true, 'async', null);
         $js = $assets->js();
         $this->assertSame($js, '<script src="/test.js" type="text/javascript" async></script>' . PHP_EOL);
+
+        $array = $assets->getJs();
+        $this->assertSame(reset($array), [
+            'asset' => '/test.js',
+            'priority' => 10,
+            'order' => 0,
+            'pipeline' => true,
+            'loading' => 'async',
+            'group' => 'head'
+        ]);
+
         $assets->reset();
         $assets->addJs('test.js', null, true, 'defer', null);
         $js = $assets->js();
         $this->assertSame($js, '<script src="/test.js" type="text/javascript" defer></script>' . PHP_EOL);
 
-
+        $array = $assets->getJs();
+        $this->assertSame(reset($array), [
+            'asset' => '/test.js',
+            'priority' => 10,
+            'order' => 0,
+            'pipeline' => true,
+            'loading' => 'defer',
+            'group' => 'head'
+        ]);
     }
 
     public function testAddingAssetPropertiesWithArray()
