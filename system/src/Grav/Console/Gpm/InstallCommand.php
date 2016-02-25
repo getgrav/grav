385c758c7783e5a0c2e3abaa73cf00e174dd1fa2
Flavio Copes <copesc@gmail.com>
2016-02-25 12:09:08 +0100

    dependencies_versions -> dependencies

--- system/src/Grav/Console/Gpm/InstallCommand.php
+++ system/src/Grav/Console/Gpm/InstallCommand.php
@@ -300,9 +300,8 @@ class InstallCommand extends ConsoleCommand
         $packageData = $this->gpm->findPackage($packageName);
 
         //Check for dependencies
-        if (isset($packageData->dependencies_versions)) {
-
-            foreach ($packageData->dependencies_versions as $dependency) {
+        if (isset($packageData->dependencies)) {
+            foreach ($packageData->dependencies as $dependency) {
                 $current_package_name = $dependency['name'];
                 if (isset($dependency['version'])) {
                     $current_package_version_information = $dependency['version'];
