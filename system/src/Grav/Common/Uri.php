a00efcfec9c6f737d143f3c6c6dac47e8c542d1f
Flavio Copes <copesc@gmail.com>
2016-04-06 10:56:21 +0200

    Fix environment configuration for localhost testing
    Also add docblock

--- system/src/Grav/Common/Uri.php
+++ system/src/Grav/Common/Uri.php
@@ -157,14 +157,26 @@ class Uri
         return $this->name;
     }
 
+    /**
+     * Return a string with the current environment name.
+     *
+     * If the server IP is a local server address (127.0.0.1 or ::1), and/or
+     * the host name is set to 'localhost', returns 'localhost'.
+     *
+     * Otherwise it returns the server name as set by self::buildHostname()
+     *
+     * @return string
+     */
     private function buildEnvironment()
     {
-        // set hostname
-        $address = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '::1';
+        $address = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '::1';
 
-        // check for localhost variations
         if ($this->name == 'localhost' || $address == '::1' || $address == '127.0.0.1') {
-            $env = 'localhost';
+            if ($this->name != 'localhost') {
+                $env = $this->name;
+            } else {
+                $env = 'localhost';
+            }
         } else {
             $env = $this->name;
         }
@@ -479,7 +491,7 @@ class Uri
      * Gets the Fragment portion of a URI (eg #target)
      *
      * @param null $fragment
-     * 
+     *
      * @return null
      */
     public function fragment($fragment = null)
