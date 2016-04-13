9c4b0b76339dbf16060c32a4bcdb31a3859ebced
Flavio Copes <copesc@gmail.com>
2016-04-13 20:13:54 +0200

    Fix issue in var name

--- system/src/Grav/Console/Gpm/InstallCommand.php
+++ system/src/Grav/Console/Gpm/InstallCommand.php
@@ -232,7 +232,7 @@ class InstallCommand extends ConsoleCommand
             $question = new ConfirmationQuestion("The package <cyan>$package_name</cyan> will be updated to a new major version <green>$new_version</green>, from <magenta>$old_version</magenta>. Be sure to read what changed with the new major release. Continue? [y|N] ", false);
 
             if (!$helper->ask($this->input, $this->output, $question)) {
-                $this->output->writeln("<yellow>Package " . $packageName . " not updated</yellow>");
+                $this->output->writeln("<yellow>Package " . $package_name . " not updated</yellow>");
                 exit;
             }
         }
