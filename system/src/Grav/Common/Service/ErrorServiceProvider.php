a56fa4b03a6a6c513a9ed0fa6dab81529d5aaed2
Oscar Recio <osrecio@gmail.com>
2016-04-21 10:28:43 +0200

    Deleted Whoops aliases (#797)
    Deleted Whoops aliases as they are in `Grav\Common\Errors` implemented. In this file are unusued

--- system/src/Grav/Common/Service/ErrorServiceProvider.php
+++ system/src/Grav/Common/Service/ErrorServiceProvider.php
@@ -4,9 +4,6 @@ namespace Grav\Common\Service;
 use Grav\Common\Errors\Errors;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
-use Whoops\Handler\JsonResponseHandler;
-use Whoops\Handler\PrettyPageHandler;
-use Whoops\Handler\PlainTextHandler;
 
 class ErrorServiceProvider implements ServiceProviderInterface
 {
