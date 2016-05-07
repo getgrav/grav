59c320eb1799786b10f4251366cfc376a5791bbb
Flavio Copes <copesc@gmail.com>
2016-05-07 10:45:07 +0200

    Also send package name

--- system/src/Grav/Common/GPM/GPM.php
+++ system/src/Grav/Common/GPM/GPM.php
@@ -209,6 +209,7 @@ class GPM extends Iterator
             if (version_compare($local_version, $remote_version) < 0) {
                 $repository[$slug]->available = $remote_version;
                 $repository[$slug]->version   = $local_version;
+                $repository[$slug]->name     = $repository[$slug]->name;
                 $repository[$slug]->type      = $repository[$slug]->release_type;
                 $items[$slug]                 = $repository[$slug];
             }
