abca8ce4339ca74fd84e3b5895f24795d7d039ac
Andy Miller <rhuk@mac.com>
2016-11-01 13:03:43 -0600

    `CompiledJsonFile::content()` should support $assoc

--- system/src/Grav/Common/File/CompiledJsonFile.php
+++ system/src/Grav/Common/File/CompiledJsonFile.php
@@ -13,4 +13,20 @@ use RocketTheme\Toolbox\File\JsonFile;
 class CompiledJsonFile extends JsonFile
 {
     use CompiledFile;
+
+    public function content($var = null, $assoc = false)
+    {
+        if ($var !== null) {
+            $this->content = $this->check($var);
+
+            // Update RAW, too.
+            $this->raw = $this->encode($this->content);
+
+        } elseif ($this->content === null) {
+            // Decode RAW file.
+            $this->content = $this->decode($this->raw(), $assoc);
+        }
+
+        return $this->content;
+    }
 }
