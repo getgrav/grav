548e207a37904c37685f395ff1c0b69fa06767c1
Andy Miller <rhuk@mac.com>
2016-12-16 10:48:55 -0700

    Reworked PHP CLI router PR #1218

--- index.php
+++ index.php
@@ -14,6 +14,12 @@ if (!is_file($autoload)) {
     die("Please run: <i>bin/grav install</i>");
 }
 
+if (PHP_SAPI == 'cli-server') {
+    if (!isset($_SERVER['PHP_CLI_ROUTER'])) {
+        die("PHP webserver requires a router to run Grav, please use: <pre>php -S {$_SERVER["SERVER_NAME"]}:{$_SERVER["SERVER_PORT"]} system/router.php</pre>");
+    }
+}
+
 use Grav\Common\Grav;
 use RocketTheme\Toolbox\Event\Event;
 
