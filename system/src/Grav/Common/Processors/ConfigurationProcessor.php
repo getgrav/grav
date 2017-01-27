b0bd38231d0e80a6060087a2dd01bff59b2add7b
Andy Miller <rhuk@mac.com>
2017-01-27 09:08:23 -0700

    Don’t set default param_sep, use system constant, but still allow override

--- system/src/Grav/Common/Processors/ConfigurationProcessor.php
+++ system/src/Grav/Common/Processors/ConfigurationProcessor.php
@@ -15,6 +15,12 @@ class ConfigurationProcessor extends ProcessorBase implements ProcessorInterface
 
     public function process() {
       	$this->container['config']->init();
+
+      	// Set param_sep based on system level PATH_SEPARATOR if not already set
+      	if (!$this->container['config']->get('system.param_sep')) {
+      	    $this->container['config']->set('system.param_sep', PATH_SEPARATOR);
+        }
+
       	return $this->container['plugins']->setup();
     }
 
