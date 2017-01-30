f287dab16d7f5b6d88240ed4b1e88d4b13426e32
Andy Miller <rhuk@mac.com>
2017-01-30 13:19:21 -0700

    Need to experiment more.. This is causing more issues than it fixes.

--- system/src/Grav/Common/Processors/ConfigurationProcessor.php
+++ system/src/Grav/Common/Processors/ConfigurationProcessor.php
@@ -8,24 +8,14 @@
 
 namespace Grav\Common\Processors;
 
-use Grav\Common\Utils;
-
-class ConfigurationProcessor extends ProcessorBase implements ProcessorInterface
-{
+class ConfigurationProcessor extends ProcessorBase implements ProcessorInterface {
 
     public $id = '_config';
     public $title = 'Configuration';
 
-    public function process()
-    {
-        $this->container['config']->init();
-
-        // Set param_sep based on system level PATH_SEPARATOR if on windows + apache
-        if ($this->container['config']->get('system.param_sep') !== PATH_SEPARATOR && Utils::isWindows() && Utils::isApache()) {
-            $this->container['config']->set('system.param_sep', PATH_SEPARATOR);
-        }
-
-        return $this->container['plugins']->setup();
+    public function process() {
+      	$this->container['config']->init();
+      	return $this->container['plugins']->setup();
     }
 
 }
