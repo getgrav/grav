24bffcde805417c3820428c35c60dbc195020104
Andy Miller <rhuk@mac.com>
2017-01-30 11:36:37 -0700

    Don’t set default param_sep, use system constant (#1275)
    * Don’t set default param_sep, use system constant, but still allow override
    
    * Automatically fix param_sep on windows
    
    * Reverted back to original system.yaml syntax
    
    * Comment fix
    
    * PSR formating fixes
    
    * Ensure automatic override of `param_sep` is only in cases where it’s different from system + windows + apache

--- system/src/Grav/Common/Processors/ConfigurationProcessor.php
+++ system/src/Grav/Common/Processors/ConfigurationProcessor.php
@@ -8,14 +8,24 @@
 
 namespace Grav\Common\Processors;
 
-class ConfigurationProcessor extends ProcessorBase implements ProcessorInterface {
+use Grav\Common\Utils;
+
+class ConfigurationProcessor extends ProcessorBase implements ProcessorInterface
+{
 
     public $id = '_config';
     public $title = 'Configuration';
 
-    public function process() {
-      	$this->container['config']->init();
-      	return $this->container['plugins']->setup();
+    public function process()
+    {
+        $this->container['config']->init();
+
+        // Set param_sep based on system level PATH_SEPARATOR if on windows + apache
+        if ($this->container['config']->get('system.param_sep') !== PATH_SEPARATOR && Utils::isWindows() && Utils::isApache()) {
+            $this->container['config']->set('system.param_sep', PATH_SEPARATOR);
+        }
+
+        return $this->container['plugins']->setup();
     }
 
 }
