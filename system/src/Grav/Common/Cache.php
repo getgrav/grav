9100c54244f18205389b2107d042746e2a227309
Andy Miller <rhuk@mac.com>
2017-02-07 13:48:50 -0700

    Fixes to allow not just ‘default/file’ cache in CLI (#1294)
    * Fixes to allow not just ‘default/file’ cache in CLI
    
    * vendor updates
    
    * Better solution
    
    * Added cli_compatibility to system blueprint
    
    * Fall back to ‘file’ if cli_compatibility and (auto or volatile)

--- system/src/Grav/Common/Cache.php
+++ system/src/Grav/Common/Cache.php
@@ -173,6 +173,12 @@ class Cache extends Getters
         $setting = $this->driver_setting;
         $driver_name = 'file';
 
+        // CLI compatibility requires a non-volatile cache driver
+        if ($this->config->get('system.cache.cli_compatibility') && (
+            $setting == 'auto' || $this->isVolatileDriver($setting))) {
+            $setting = $driver_name;
+        }
+
         if (!$setting || $setting == 'auto') {
             if (extension_loaded('apcu')) {
                 $driver_name = 'apcu';
@@ -436,5 +442,40 @@ class Cache extends Getters
         }
 
         return $this->lifetime;
+    }
+
+    /**
+     * Returns the current driver name
+     *
+     * @return mixed
+     */
+    public function getDriverName()
+    {
+        return $this->driver_name;
+    }
+
+    /**
+     * Returns the current driver setting
+     *
+     * @return mixed
+     */
+    public function getDriverSetting()
+    {
+        return $this->driver_setting;
+    }
+
+    /**
+     * is this driver a volatile driver in that it resides in PHP process memory
+     *
+     * @param $setting
+     * @return bool
+     */
+    public function isVolatileDriver($setting)
+    {
+        if (in_array($setting, ['apc', 'apcu', 'xcache', 'wincache'])) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
