c7bcbeabd9b284503ef61b812b815893fa90af31
Matias Griese <matias@kunena.org>
2017-05-16 12:00:49 +0300

    Add support for loading properties in Object class

--- system/src/Grav/Framework/Object/Object.php
+++ system/src/Grav/Framework/Object/Object.php
@@ -18,7 +18,10 @@ use RocketTheme\Toolbox\ArrayTraits\Export;
  */
 class Object implements ObjectInterface
 {
-    use ObjectTrait, ArrayAccessWithGetters, Export;
+    use ObjectTrait, ArrayAccessWithGetters, Export {
+        ArrayAccessWithGetters::offsetExists as private parentOffsetExists;
+        ArrayAccessWithGetters::offsetGet as private parentOffsetGet;
+    }
 
     /**
      * Properties of the object.
@@ -44,6 +47,32 @@ class Object implements ObjectInterface
         if ($this->key === null) {
             throw new \InvalidArgumentException('Object cannot be created without assigning a key');
         }
+    }
+
+    /**
+     * Checks whether or not an offset exists with a possibility to load the field by $this->loadField{offset}().
+     *
+     * @param mixed $offset  An offset to check for.
+     * @return bool          Returns TRUE on success or FALSE on failure.
+     */
+    public function offsetExists($offset)
+    {
+        return $this->parentOffsetExists($offset) || method_exists($this, "loadOffset_{$offset}");
+    }
+
+    /**
+     * Returns the value at specified offset with a possibility to load the field by $this->loadField{offset}().
+     *
+     * @param mixed $offset  The offset to retrieve.
+     * @return mixed         Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        if (!$this->parentOffsetExists($offset) && method_exists($this, "loadOffset_{$offset}")) {
+            $this->offsetSet($offset, call_user_func([$this, "loadOffset_{$offset}"]));
+        }
+
+        return $this->parentOffsetGet($offset);
     }
 
     /**
