208c985ca0cdb1b469de01c9fcd09cbceb9eaccd
Djamil Legato <djamil+github@djamil.it>
2017-07-10 11:31:23 -0700

    Fix natural sort when > 100 pages

--- system/src/Grav/Common/Page/Pages.php
+++ system/src/Grav/Common/Page/Pages.php
@@ -1240,7 +1240,7 @@ class Pages
                 case 'default':
                 default:
                     $list[$key] = $key;
-                    $sort_flags = $sort_flags ?: SORT_REGULAR;
+                    $sort_flags = $sort_flags ?: SORT_NATURAL | SORT_FLAG_CASE;
             }
         }
 
@@ -1257,6 +1257,12 @@ class Pages
                 $locale = setlocale(LC_COLLATE, 0); //`setlocale` with a 0 param returns the current locale set
                 $col = Collator::create($locale);
                 if ($col) {
+                    if (($sort_flags & SORT_NATURAL) === SORT_NATURAL) {
+                        $list = preg_replace_callback('~([0-9]+)~', function($number) {
+                            return sprintf('%032d', $number);
+                        }, $list);
+                    }
+
                     $col->asort($list, $sort_flags);
                 } else {
                     asort($list, $sort_flags);
