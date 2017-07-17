ae88d9b66e75c6c64981eabe4e00bc3d2bc81878
Flavio Copes <copesc@gmail.com>
2017-07-17 16:25:59 +0200

    Fix ordering in linux+intl environments

--- system/src/Grav/Common/Page/Pages.php
+++ system/src/Grav/Common/Page/Pages.php
@@ -1259,7 +1259,7 @@ class Pages
                 if ($col) {
                     if (($sort_flags & SORT_NATURAL) === SORT_NATURAL) {
                         $list = preg_replace_callback('~([0-9]+)\.~', function($number) {
-                            return sprintf('%032d', $number);
+                            return sprintf('%032d', $number[0]);
                         }, $list);
                     }
 
