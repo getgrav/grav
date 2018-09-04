1faf364e99b2e80cc5fe40d8a7f2d89e46cbf049
Andy Miller <rhuk@mac.com>
2018-09-04 15:24:47 -0600

    Revamped with Symfony 4.1 CLI updates

--- system/src/Grav/Console/Cli/SchedulerCommand.php
+++ system/src/Grav/Console/Cli/SchedulerCommand.php
@@ -8,22 +8,27 @@
 
 namespace Grav\Console\Cli;
 
+use Cron\CronExpression;
 use Grav\Common\Grav;
 use Grav\Common\Scheduler\Scheduler;
 use Grav\Console\ConsoleCommand;
 use RocketTheme\Toolbox\Event\Event;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputOption;
-use League\CLImate\CLImate;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class SchedulerCommand extends ConsoleCommand
 {
-    /**
-     *
-     */
     protected function configure()
     {
         $this
             ->setName('scheduler')
+            ->addOption(
+                'install',
+                'i',
+                InputOption::VALUE_NONE,
+                'Show Install Command'
+            )
             ->addOption(
                 'jobs',
                 'j',
@@ -31,10 +36,10 @@ class SchedulerCommand extends ConsoleCommand
                 'Show Jobs Summary'
             )
             ->addOption(
-                'errors',
-                'e',
+                'details',
+                'd',
                 InputOption::VALUE_NONE,
-                'Show Errors'
+                'Show Job Details'
             )
             ->setDescription('Run the Grav Scheduler.  Best when integrated with system cron')
             ->setHelp("Running without any options will force the Scheduler to run through it's jobs and process them");
@@ -61,76 +66,105 @@ class SchedulerCommand extends ConsoleCommand
         $scheduler = $grav['scheduler'];
         $grav->fireEvent('onSchedulerInitialized', new Event(['scheduler' => $scheduler]));
 
+        $this->setHelp('foo');
+
+        /** @var use new SymfonyStyle helper $io */
+        $io = new SymfonyStyle($this->input, $this->output);
+
         if ($this->input->getOption('jobs')) {
             // Show jobs list
-            $climate = new CLImate;
-            $climate->extend('Grav\Console\TerminalObjects\Table');;
 
             $jobs = $scheduler->getAllJobs();
-            $table = [];
-
-            $this->output->writeln('');
-            $this->output->writeln('<magenta>Scheduler Jobs Listing</magenta>');
-            $this->output->writeln('');
-
             $job_states = $scheduler->getJobStates()->content();
+            $rows = [];
+
+            $table = new Table($this->output);
+            $table->setStyle('box');
+            $headers = ['Job ID', 'Command', 'Run At', 'Status', 'Last Run', 'State'];
+
+            $io->title('Scheduler Jobs Listing');
 
             foreach ($jobs as $job) {
                 $job_status = ucfirst($job_states[$job->getId()]['state'] ?? 'ready');
                 $last_run = $job_states[$job->getId()]['last-run'] ?? 0;
                 $status = $job_status === 'Failure' ? "<red>{$job_status}</red>" : "<green>{$job_status}</green>";
-                $state = $job->getEnabled() ? "<blue>Enabled</blue>" : "Disabled";
+                $state = $job->getEnabled() ? "<cyan>Enabled</cyan>" : "<red>Disabled</red>";
                 $row = [
-                    'ID' => $job->getId(),
-                    'Command' => "<white><bold>{$job->getCommand()}</bold></white>",
-                    'Run At' => "<magenta>{$job->getAt()}</magenta>",
-                    'Status' => "<bold>{$status}</bold>",
-                    "Last Run" => "<yellow>" . ($last_run === 0 ? 'Never' : date('Y-m-d H:i', $last_run)) . "</yellow>",
-                    'State' => $state,
+                    $job->getId(),
+                    "<white>{$job->getCommand()}</white>",
+                    "<magenta>{$job->getAt()}</magenta>",
+                    "{$status}",
+                    "<yellow>" . ($last_run === 0 ? 'Never' : date('Y-m-d H:i', $last_run)) . "</yellow>",
+                    $state,
 
                 ];
-                $table[] = $row;
+                $rows[] = $row;
             }
 
-            if (!empty($table)) {
-                $climate->table($table);
+            if (!empty($rows)) {
+                $table->setHeaders($headers);
+                $table->setRows($rows);
+                $table->render();
             } else {
-                $this->output->writeln('no jobs found...');
+                $io->text('no jobs found...');
             }
 
-
-            $this->output->writeln('');
-            $this->output->writeln('<yellow>NOTE: For error details run "bin/grav scheduler -e"</yellow>');
-            $this->output->writeln('');
-        } elseif ($this->input->getOption('errors')) {
-            $this->output->writeln('');
-            $this->output->writeln('<magenta>Job Errors</magenta>');
-            $this->output->writeln('');
-
+            $io->newLine();
+            $io->note('For error details run "bin/grav scheduler -d"');
+            $io->newLine();
+        } elseif ($this->input->getOption('details')) {
             $jobs = $scheduler->getAllJobs();
             $job_states = $scheduler->getJobStates()->content();
 
+            $io->title('Job Details');
+
+            $table = new Table($this->output);
+            $table->setStyle('box');
+            $table->setHeaders(['Job ID', 'Last Run', 'Next Run', 'Errors']);
+            $rows = [];
+
             foreach ($jobs as $job) {
                 $job_state = $job_states[$job->getId()];
-                if (isset($job_state['error'])) {
-                    $this->output->writeln("Job ID:    {$job->getId()}");
-                    $this->output->writeln("Command:   <white>{$job->getCommand()}</white>");
-                    $this->output->writeln("Last Run:  <yellow>" . date('Y-m-d H:i', $job_state['last-run']) . "</yellow>");
-                    $this->output->writeln("Error Msg: <red>{$job_state['error']}</red>");
-                    $this->output->writeln('');
+                $error = isset($job_state['error']) ? trim($job_state['error']) : false;
+
+                /** @var CronExpression $expression */
+                $expression = $job->getCronExpression();
+                $next_run = $expression->getNextRunDate();
+
+                $row = [];
+                $row[] = $job->getId();
+                $row[] = "<yellow>" . date('Y-m-d H:i', $job_state['last-run']) . "</yellow>";
+                $row[] = "<yellow>" . $next_run->format('Y-m-d H:i') . "</yellow>";
+
+                if ($error) {
+                    $row[] = "<error>{$error}</error>";
+                } else {
+                    $row[] = "<green>None</green>";
                 }
+                $rows[] = $row;
             }
 
+            $table->setRows($rows);
+            $table->render();
+
+        } elseif ($this->input->getOption('install')) {
+            $io->title('Install Scheduler');
+
+            if ($scheduler->isCrontabSetup()) {
+                $io->success('All Ready! You have already set up Grav\'s Scheduler in your crontab');
+            } else {
+                $io->error('You still need to set up Grav\'s Scheduler in your crontab');
+            }
+            $io->note('To install, run the following command from your terminal:');
+            $io->newLine();
+            $io->text(trim($scheduler->getCronCommand()));
         } else {
             // Run scheduler
             $scheduler->run();
 
             if ($this->input->getOption('verbose')) {
-                $this->output->writeln('');
-                $this->output->writeln('<magenta>Running Scheduled Jobs</magenta>');
-                $this->output->writeln('');
-                $output = $scheduler->getVerboseOutput();
-                $this->output->writeln($output);
+                $io->title('Running Scheduled Jobs');
+                $io->text($scheduler->getVerboseOutput());
             }
         }
     }
