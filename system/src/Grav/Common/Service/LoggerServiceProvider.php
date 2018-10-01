70b6fb32c4af52187a68a24690a009194b31eebd
Andy Miller <rhuk@mac.com>
2018-10-01 15:11:13 -0600

    Revert "Splitted log handler and added support for syslog along side with stream (file)"
    This reverts commit ecaeb32675b39c1bacc03511b1c0632e2337d385.

--- system/src/Grav/Common/Service/LoggerServiceProvider.php
+++ system/src/Grav/Common/Service/LoggerServiceProvider.php
@@ -8,8 +8,6 @@
 
 namespace Grav\Common\Service;
 
-use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\SyslogHandler;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 use \Monolog\Logger;
@@ -21,29 +19,14 @@ class LoggerServiceProvider implements ServiceProviderInterface
     public function register(Container $container)
     {
         $container['log'] = function ($c) {
-            /** @var Config $config */
-            $config = $c['config'];
-            $handler = $config->get('system.log.handler', 'file');
-
             $log = new Logger('grav');
 
             /** @var UniformResourceLocator $locator */
             $locator = $c['locator'];
 
-            switch ($handler) {
-                case 'syslog':
-                    $facility = $config->get('system.log.syslog.facility', 'local6');
-                    $logHandler = new SyslogHandler('grav', $facility);
-                    $formatter = new LineFormatter("%channel%.%level_name%: %message% %extra%");
-                    $logHandler->setFormatter($formatter);
-                    break;
-                case 'file':
-                default:
-                    $log_file = $locator->findResource('log://grav.log', true, true);
-                    $logHandler = new StreamHandler($log_file, Logger::DEBUG);
-            }
+            $log_file = $locator->findResource('log://grav.log', true, true);
 
-            $log->pushHandler($logHandler);
+            $log->pushHandler(new StreamHandler($log_file, Logger::DEBUG));
 
             return $log;
         };
