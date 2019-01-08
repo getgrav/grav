0bb85f2d866e5e65f32d16c7409c7f39c597928c
Matias Griese <matias@trilbymedia.com>
2019-01-08 18:30:01 +0200

    Framework: update copyrights to 2019

--- system/src/Grav/Framework/File/AbstractFile.php
+++ system/src/Grav/Framework/File/AbstractFile.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * @package    Grav\Framework\File
  *
- * @copyright  Copyright (C) 2015 - 2018 Trilby Media, LLC. All rights reserved.
+ * @copyright  Copyright (C) 2015 - 2019 Trilby Media, LLC. All rights reserved.
  * @license    MIT License; see LICENSE file for details.
  */
 
