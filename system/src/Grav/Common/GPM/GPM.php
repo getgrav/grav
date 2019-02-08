48816d9f2e0d8004717b86b2a02852b22ab5198e
Andy Miller <rhuk@mac.com>
2019-02-07 17:56:45 -0700

    Fix issue with vUndefined in GPM

--- system/src/Grav/Common/GPM/GPM.php
+++ system/src/Grav/Common/GPM/GPM.php
@@ -251,7 +251,7 @@ class GPM extends Iterator
                 $repository[$slug]->version = $local_version;
                 $repository[$slug]->name = $repository[$slug]->name;
                 $repository[$slug]->type = $repository[$slug]->release_type;
-                $items[$slug] = $repository[$slug];
+                $items[$slug] = $repository[$slug]->toArray();
             }
         }
 
@@ -330,7 +330,7 @@ class GPM extends Iterator
                 $repository[$slug]->available = $remote_version;
                 $repository[$slug]->version = $local_version;
                 $repository[$slug]->type = $repository[$slug]->release_type;
-                $items[$slug] = $repository[$slug];
+                $items[$slug] = $repository[$slug]->toArray();
             }
         }
 
