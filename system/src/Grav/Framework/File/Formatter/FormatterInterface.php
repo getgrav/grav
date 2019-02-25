61cf416df4053aff6d6da0eeaf1a0eff120bbc3d
Matias Griese <matias@trilbymedia.com>
2019-02-25 11:43:42 +0200

    Added B/C layer for FormatterInterface

---
+++ system/src/Grav/Framework/File/Formatter/FormatterInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Grav\Framework\File\Formatter;
+
+use Grav\Framework\File\Interfaces\FileFormatterInterface;
+
+/**
+ * @deprecated 1.6 Use Grav\Framework\File\Interfaces\FileFormatterInterface instead
+ */
+interface FormatterInterface extends FileFormatterInterface {};
