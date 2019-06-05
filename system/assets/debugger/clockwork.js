72d012e401eef19d263c1b978543a5ce834286a4
Andy Miller <rhuk@mac.com>
2019-06-04 20:11:41 -0600

    minified 2

--- system/assets/debugger/clockwork.js
+++ system/assets/debugger/clockwork.js
@@ -1,6 +1,2 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var el = document.createElement('div');
-    el.appendChild(document.createElement('i'));
-    el.className = 'clockwork-badge';
-    document.body.appendChild(el);
-});
+/** Clockwork Debugger JS **/
+document.addEventListener("DOMContentLoaded",function(){var e=document.createElement("div");e.appendChild(document.createElement("i")),e.className="clockwork-badge",document.body.appendChild(e)});
