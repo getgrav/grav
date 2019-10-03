7dafa2a207236b7b4c9e5cbb41bdecb03a5cb6ba
Andy Miller <rhuk@mac.com>
2019-10-03 14:31:28 -0600

    prepare for beta.10 release

--- system/defines.php
+++ system/defines.php
@@ -8,7 +8,7 @@
 
 // Some standard defines
 define('GRAV', true);
-define('GRAV_VERSION', '1.7.0-beta.9');
+define('GRAV_VERSION', '1.7.0-beta.10');
 define('GRAV_TESTING', true);
 define('DS', '/');
 
