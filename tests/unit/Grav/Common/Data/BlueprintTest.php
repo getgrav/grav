5f0380f5470aab18fff2c49a86b7ee4686e1f1df
Matias Griese <matias@trilbymedia.com>
2020-05-05 19:04:56 +0300

    Fixed unit test

--- tests/unit/Grav/Common/Data/BlueprintTest.php
+++ tests/unit/Grav/Common/Data/BlueprintTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use Grav\Common\Config\Config;
 use Grav\Common\Data\Blueprint;
+use Grav\Common\Grav;
 
 /**
  * Class InstallCommandTest
@@ -29,13 +31,30 @@ class BlueprintTest extends \Codeception\TestCase\Test
 
     /**
      * @depends testValidateStrict
-     * @expectedException Grav\Common\Data\ValidationException
      */
     public function testValidateStrictExtra()
     {
         $blueprint = $this->loadBlueprint('strict');
 
         $blueprint->validate(['test' => 'string', 'wrong' => 'field']);
+    }
+
+    /**
+     * @depends testValidateStrict
+     * @expectedException Grav\Common\Data\ValidationException
+     */
+    public function testValidateStrictExtraException()
+    {
+        $blueprint = $this->loadBlueprint('strict');
+
+        /** @var Config $config */
+        $config = Grav::instance()['config'];
+        $var = 'system.strict_mode.blueprint_strict_compat';
+        $config->set($var, false);
+
+        $blueprint->validate(['test' => 'string', 'wrong' => 'field']);
+
+        $config->set($var, true);
     }
 
     /**
