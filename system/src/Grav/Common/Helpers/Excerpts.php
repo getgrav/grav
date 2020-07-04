86edc18f214d406244303ce9c563fe2bf76bb16a
Andy Miller <rhuk@mac.com>
2020-07-03 21:19:39 -0600

    Safer solution for getExcerptsFromHtml
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- system/src/Grav/Common/Helpers/Excerpts.php
+++ system/src/Grav/Common/Helpers/Excerpts.php
@@ -74,7 +74,7 @@ class Excerpts
     {
         $doc = new \DOMDocument('1.0', 'UTF-8');
         $internalErrors = libxml_use_internal_errors(true);
-        $doc->loadHTML('<?xml encoding="utf-8" ?>' . $html);
+        $doc->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
         libxml_use_internal_errors($internalErrors);
 
         $elements = $doc->getElementsByTagName($tag);
