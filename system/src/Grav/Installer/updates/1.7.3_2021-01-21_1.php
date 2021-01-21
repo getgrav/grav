625aa3f1bd144def2529c57b43c8a88212d2346f
Matias Griese <matias@trilbymedia.com>
2021-01-21 23:00:34 +0200

    Fixed a bug in post-install methods

--- system/src/Grav/Installer/updates/1.7.3_2021-01-21_1.php
+++ system/src/Grav/Installer/updates/1.7.3_2021-01-21_1.php
@@ -17,8 +17,10 @@ return [
             try {
                 // Keep old defaults for backwards compatibility.
                 $yaml = YamlUpdater::instance(GRAV_ROOT . '/user/config/system.yaml');
-                $yaml->undefine('gpm.releases');
-                $yaml->save();
+                if (!$yaml->isHandWritten()) {
+                    $yaml->undefine('gpm.releases');
+                    $yaml->save();
+                }
             } catch (\Exception $e) {
                 throw new InstallException('Could not update system configuration to maintain backwards compatibility', $e);
             }
