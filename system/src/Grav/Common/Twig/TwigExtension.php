aeb2e3a7155541333d6a4927c02ab6bcdcc22b18
Matias Griese <matias@trilbymedia.com>
2021-04-13 21:11:05 +0300

    Add back alias for moved class

---
+++ system/src/Grav/Common/Twig/TwigExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @package    Grav\Common\Twig
+ *
+ * @copyright  Copyright (c) 2015 - 2021 Trilby Media, LLC. All rights reserved.
+ * @license    MIT License; see LICENSE file for details.
+ */
+
+namespace Grav\Common\Twig;
+
+use Grav\Common\Twig\Extension\GravExtension;
+
+/**
+ * Class TwigExtension
+ * @package Grav\Common\Twig
+ * @deprecated 1.7 Use GravExtension instead
+ */
+class TwigExtension extends GravExtension
+{
+}
