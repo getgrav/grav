2074c46e2dcf183a0e9178929a2c91407bd04a58
Ulysse Gérard <thevoodoos@gmail.com>
2021-05-28 19:09:31 +0200

    Setup toolchain to build JS modules

---
+++ user/themes/afj/gulpfile.esm.js
@@ -0,0 +1,82 @@
+/* WE USE
+    - sass for scss compilation
+    - rollup-stream for js bundling
+    - terser for js minification
+*/
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import cleancss from 'gulp-clean-css';
+import csscomb from 'gulp-csscomb';
+import rename from 'gulp-rename';
+import autoprefixer from 'gulp-autoprefixer';
+
+import rollupStream from '@rollup/stream';
+import buffer from 'vinyl-buffer';
+import sourcemaps from 'gulp-sourcemaps';
+import source from 'vinyl-source-stream';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import css from 'rollup-plugin-css-porter';
+
+import terser from 'gulp-terser';
+
+import { parallel, series } from 'gulp';
+
+// configure the paths
+let scss_watch_dir = ['./scss/**/*.scss'];
+let scss_src_dir = './scss/*.scss';
+let css_dest_dir = './css-compiled';
+
+// all javascript using modules must be in the `js-es6` folder
+let js_watch_dir = ['./js-es6/**/*.js'];
+// let js_files = []
+
+let rollup = js_file => {
+    const options = {
+        input: `./js-es6/${js_file}.js`,
+        plugins: [
+            css({dest: `./css-compiled/${js_file}.css`, raw: false}),
+            commonjs(),
+            nodeResolve()
+        ],
+        output: {
+            format: 'iife',
+            sourcemap: true
+        } };
+    return rollupStream(options)
+      .pipe(source(`${js_file}.js`))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(terser())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('js-compiled'));
+};
+
+let javascript = () => rollup('calendar');
+
+let scss = () =>
+    gulp.src(scss_src_dir)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+                outputStyle: 'compact',
+                precision: 10
+            }).on('error', sass.logError)
+        )
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(css_dest_dir))
+        .pipe(csscomb())
+        .pipe(cleancss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(css_dest_dir));
+
+let watch_scss = () => gulp.watch(scss_watch_dir, scss);
+let watch_js = () =>  gulp.watch(js_watch_dir, javascript);
+
+let build = parallel(scss, javascript);
+
+exports.watch = series(build, parallel(watch_js, watch_scss));
+exports.build = build;
+exports.default = build;
