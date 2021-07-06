7e152709974e356b7439bcff8b23925f74145d5f
Karmalakas <cronus.raven@gmail.com>
2021-07-06 21:14:06 +0300

    Add `setCurrent()` method to Page Collection
    #531

--- system/src/Grav/Common/Page/Collection.php
+++ system/src/Grav/Common/Page/Collection.php
@@ -146,6 +146,18 @@ class Collection extends Iterator implements PageCollectionInterface
     }
 
     /**
+     * Set current page.
+     */
+    public function setCurrent(string $path): void
+    {
+        reset($this->items);
+
+        while (key($this->items) !== $path && key($this->items) !== null) {
+            next($this->items);
+        }
+    }
+
+    /**
      * Returns current page.
      *
      * @return PageInterface
