777ac119def54f2cff03638bbe9463bf1fcf7069
Matias Griese <matias@trilbymedia.com>
2021-10-20 20:33:05 +0300

    Update all libraries (WIP)

---
+++ system/src/Rockettheme/Event/Event.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RocketTheme\Toolbox\Event;
+
+use RocketTheme\Toolbox\ArrayTraits\ArrayAccess;
+use RocketTheme\Toolbox\ArrayTraits\Constructor;
+use RocketTheme\Toolbox\ArrayTraits\Export;
+
+/**
+ * Implements Symfony Event interface.
+ *
+ * @package RocketTheme\Toolbox\Event
+ * @author RocketTheme
+ * @license MIT
+ * @deprecated Event classes will be removed in the future. Use PSR-14 implementation instead.
+ */
+class Event extends \Symfony\Component\EventDispatcher\Event implements \ArrayAccess
+{
+    use ArrayAccess, Constructor, Export;
+
+    /** @var array */
+    protected array $items = [];
+}
