593bf98e9d07599e505ea5304dc8958381c301e8
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2022-02-18 13:53:34 +0100

    Remove variable duplicate

--- system/src/Grav/Common/Backup/Backups.php
+++ system/src/Grav/Common/Backup/Backups.php
@@ -104,7 +104,7 @@ class Backups
      */
     public function getBackupDownloadUrl($backup, $base_url)
     {
-        $param_sep = $param_sep = Grav::instance()['config']->get('system.param_sep', ':');
+        $param_sep = Grav::instance()['config']->get('system.param_sep', ':');
         $download = urlencode(base64_encode(Utils::basename($backup)));
         $url      = rtrim(Grav::instance()['uri']->rootUrl(true), '/') . '/' . trim(
             $base_url,
