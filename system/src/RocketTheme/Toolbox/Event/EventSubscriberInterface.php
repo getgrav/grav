f06bbfc5630961ccfd45b050a7d8ad1e340f5b89
Andy Miller <rhuk@mac.com>
2024-10-25 20:17:41 +0100

    Missing RocketTheme\Toolbox\Event\EventSubscriberInterface

---
+++ system/src/RocketTheme/Toolbox/Event/EventSubscriberInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace RocketTheme\Toolbox\Event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface as BaseEventSubscriberInterface;
+
+/**
+ * Defines EventSubscriberInterface.
+ *
+ * @package RocketTheme\Toolbox\Event
+ * @author RocketTheme
+ * @license MIT
+ * @deprecated Event classes will be removed in the future. Use PSR-14 implementation instead.
+ */
+interface EventSubscriberInterface extends BaseEventSubscriberInterface
+{
+}
