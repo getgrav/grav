8a10d6bc5417ac940b373d520dbd39dfc20e5bcd
Andy Miller <rhuk@mac.com>
2025-10-19 18:05:48 -0600

    fixing tests
    Signed-off-by: Andy Miller <rhuk@mac.com>

--- system/src/Grav/Common/Upgrade/SafeUpgradeService.php
+++ system/src/Grav/Common/Upgrade/SafeUpgradeService.php
@@ -418,15 +418,9 @@ class SafeUpgradeService
      */
     public function clearRecoveryFlag(): void
     {
-        $paths = [
-            $this->rootPath . '/user/data/recovery.flag',
-            $this->rootPath . '/user/data/recovery.window',
-        ];
-
-        foreach ($paths as $path) {
-            if (is_file($path)) {
-                @unlink($path);
-            }
+        $flag = $this->rootPath . '/user/data/recovery.flag';
+        if (is_file($flag)) {
+            @unlink($flag);
         }
     }
 
