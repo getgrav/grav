0512f65b1e5f0c25ce6d0201a7290a9680fbbdc5
Andy Miller <rhuk@mac.com>
2026-09-10 21:08:44 -0600

    PHPStan 2: replace the removed check* options with their error identifiers, and selectSingle() with selectFromArgs() in Grav's extensions

--- tests/phpstan/classes/Toolbox/UniformResourceLocatorExtension.php
+++ tests/phpstan/classes/Toolbox/UniformResourceLocatorExtension.php
@@ -46,6 +46,6 @@ class UniformResourceLocatorExtension implements DynamicMethodReturnTypeExtensio
             return new StringType();
         }
 
-        return ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
+        return ParametersAcceptorSelector::selectFromArgs($scope, $methodCall->getArgs(), $methodReflection->getVariants())->getReturnType();
     }
 }
